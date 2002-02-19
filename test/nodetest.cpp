366d191cb424528284fa7bdb00ab9bec8a23820f
allenb <allenb@bdc45a6f-1a19-0410-8385-c3db3a2b84f1>
2002-02-19 18:46:34 +0000

    Added some test files.  We may want to expand this to an actual test suite in the future, but this will work for now.
    git-svn-id: https://xml-cppdom.svn.sourceforge.net/svnroot/xml-cppdom/trunk/cppdom@11 bdc45a6f-1a19-0410-8385-c3db3a2b84f1

---
+++ test/nodetest.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include "xmlpp.h"
+
+bool configureInput( const std::string& filename ) 
+{
+   xmlpp::XMLContextPtr ctx( new xmlpp::XMLContext );
+   xmlpp::XMLDocument doc( ctx );
+   
+   // load a xml document from a file
+   try 
+   { 
+      doc.load_file( filename );
+   }
+   catch (xmlpp::xmlerror e) 
+   {
+      std::cerr << "Error: " << e.get_string() << std::endl;
+      if (e.get_info().size()) 
+      {
+         std::cerr << "File: " << e.get_info() << std::endl;
+      }
+      if (e.get_error() != xmlpp::xml_filename_invalid &&
+          e.get_error() != xmlpp::xml_file_access) 
+      {
+         e.show_error( ctx );
+         e.show_line( ctx, filename );
+      }
+      return false;
+   }
+   
+   std::cerr << "succesfully loaded " << filename << std::endl;
+   
+   xmlpp::XMLNodeList nl = doc.firstchild( "gameinput" )->children();
+   xmlpp::XMLNodeListIterator it = nl.begin();
+   while (it != nl.end())
+   { 
+      std::cerr << "in name: " << (*it)->name() << std::endl;
+      try
+      {
+         xmlpp::XMLAttributes& attr = (*it)->get_attrmap();
+         std::cout << "attr: " << attr.get( "action" ) << "\n" << std::flush;
+         std::cout << "attr: " << attr.get( "device" ) << "\n" << std::flush;
+         std::cout << "attr: " << attr.get( "input" ) << "\n" << std::flush;
+      } 
+      catch (xmlpp::xmlerror e)
+      {
+         std::cerr << "Error: " << e.get_string() << std::endl;
+         it++;
+         continue;
+      }
+      it++;   
+   }
+
+   return true;
+}
+
+void main()
+{
+   configureInput( "hamlet.xml" );
+}
