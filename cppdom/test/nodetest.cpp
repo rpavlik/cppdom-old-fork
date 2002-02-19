1f5dd708fff329a77ee71917b37432b506074a51
allenb <allenb@bdc45a6f-1a19-0410-8385-c3db3a2b84f1>
2002-02-19 23:09:37 +0000

    Added comments.
    - Starting to convert all comment to JavaDoc format.
    
    
    git-svn-id: https://xml-cppdom.svn.sourceforge.net/svnroot/xml-cppdom/trunk@19 bdc45a6f-1a19-0410-8385-c3db3a2b84f1

--- cppdom/test/nodetest.cpp
+++ cppdom/test/nodetest.cpp
@@ -2,25 +2,25 @@
 #include <string>
 #include <xmlpp/xmlpp.h>
 
-bool configureInput( const std::string& filename ) 
+bool configureInput( const std::string& filename )
 {
    xmlpp::XMLContextPtr ctx( new xmlpp::XMLContext );
    xmlpp::XMLDocument doc( ctx );
-   
+
    // load a xml document from a file
-   try 
-   { 
+   try
+   {
       doc.load_file( filename );
    }
-   catch (xmlpp::xmlerror e) 
+   catch (xmlpp::xmlerror e)
    {
       std::cerr << "Error: " << e.get_string() << std::endl;
-      if (e.get_info().size()) 
+      if (e.get_info().size())
       {
          std::cerr << "File: " << e.get_info() << std::endl;
       }
       if (e.get_error() != xmlpp::xml_filename_invalid &&
-          e.get_error() != xmlpp::xml_file_access) 
+          e.get_error() != xmlpp::xml_file_access)
       {
 /*
          e.show_error( ctx );
@@ -30,13 +30,13 @@ bool configureInput( const std::string& filename )
       }
       return false;
    }
-   
+
    std::cerr << "succesfully loaded " << filename << std::endl;
-   
+
    xmlpp::XMLNodeList nl = doc.firstchild( "gameinput" )->children();
    xmlpp::XMLNodeListIterator it = nl.begin();
    while (it != nl.end())
-   { 
+   {
       std::cerr << "in name: " << (*it)->name() << std::endl;
       try
       {
@@ -44,21 +44,23 @@ bool configureInput( const std::string& filename )
          std::cout << "attr: " << attr.get( "action" ) << "\n" << std::flush;
          std::cout << "attr: " << attr.get( "device" ) << "\n" << std::flush;
          std::cout << "attr: " << attr.get( "input" ) << "\n" << std::flush;
-      } 
+      }
       catch (xmlpp::xmlerror e)
       {
          std::cerr << "Error: " << e.get_string() << std::endl;
          it++;
          continue;
       }
-      it++;   
+      it++;
    }
 
    return true;
 }
 
+/** Main function */
 int main()
 {
+   // Just call single function to load and process input file
    configureInput( "hamlet.xml" );
    return 1;
 }
