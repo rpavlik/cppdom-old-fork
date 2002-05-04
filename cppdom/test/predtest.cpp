60cfadec6006ec5d413a4d353ae86e9ba17693aa
subatomic <subatomic@bdc45a6f-1a19-0410-8385-c3db3a2b84f1>
2002-05-04 21:31:33 +0000

    implemented predicate tests.  passes.
    git-svn-id: https://xml-cppdom.svn.sourceforge.net/svnroot/xml-cppdom/trunk@54 bdc45a6f-1a19-0410-8385-c3db3a2b84f1

--- cppdom/test/predtest.cpp
+++ cppdom/test/predtest.cpp
@@ -2,6 +2,47 @@
 
 int main()
 {
-   xmlpp::AttributeExists e;
-   xmlpp::AttributeEqualToValue v;
+   xmlpp::XMLContextPtr ctx( new xmlpp::XMLContext );
+   xmlpp::XMLDocument doc( ctx );
+   std::string filename = "hamlet.xml";
+   
+   // load a xml document from a file
+   try
+   {
+      doc.load_file( filename );
+   }
+   catch (xmlpp::xmlerror e)
+   {
+      std::cerr << "Error: " << e.get_string() << std::endl;
+      if (e.get_info().size())
+      {
+         std::cerr << "File: " << e.get_info() << std::endl;
+      }
+      if (e.get_error() != xmlpp::xml_filename_invalid &&
+          e.get_error() != xmlpp::xml_file_access)
+      {
+/*
+         e.show_error( ctx );
+         e.show_line( ctx, filename );
+*/
+         std::cout << "Error: (need to impl the show functions)" << std::endl;
+      }
+      assert( false && "test failed" );
+      return false;
+   }
+
+   xmlpp::XMLNodePtr root = doc.getChild( "gameinput" );
+
+      
+
+   // get all nodes with noattrs attributes...
+   xmlpp::XMLNodeList nl = root->getChildrenPred( xmlpp::AttributeType( "noattrs" ) );
+   
+   assert( nl.size() == 1 && "test failed" );
+   
+   // get all the nodes that set up Mouse devices
+   nl = root->getChildrenPred( xmlpp::AttributeValue( "device", "Mouse" ) );
+   assert( nl.size() == 2 && "test failed" );
+  
+   std::cout << "Tests Passed" << std::endl;
 }
