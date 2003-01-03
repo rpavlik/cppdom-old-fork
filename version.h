b4a41453fdd4fdab1e1aa01961c4085f4d626cd1
nonchocoboy <nonchocoboy@bdc45a6f-1a19-0410-8385-c3db3a2b84f1>
2003-01-03 03:14:37 +0000

    Bumped version to 0.1.0
    git-svn-id: https://xml-cppdom.svn.sourceforge.net/svnroot/xml-cppdom/trunk/cppdom/cppdom@212 bdc45a6f-1a19-0410-8385-c3db3a2b84f1

--- version.h
+++ version.h
@@ -54,8 +54,8 @@
  */
 // The major/minor/patch version (up to 3 digits each).
 #define CPPDOM_VERSION_MAJOR     0
-#define CPPDOM_VERSION_MINOR     0
-#define CPPDOM_VERSION_PATCH     1
+#define CPPDOM_VERSION_MINOR     1
+#define CPPDOM_VERSION_PATCH     0
 
 //--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
