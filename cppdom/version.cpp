c8fcc1698a8b127b5c2cb4be9aff951e58aa731e
nonchocoboy <nonchocoboy@bdc45a6f-1a19-0410-8385-c3db3a2b84f1>
2003-01-03 02:37:47 +0000

    Use auto-copyright on the files.
    git-svn-id: https://xml-cppdom.svn.sourceforge.net/svnroot/xml-cppdom/trunk/cppdom@208 bdc45a6f-1a19-0410-8385-c3db3a2b84f1

--- cppdom/version.cpp
+++ cppdom/version.cpp
@@ -1,30 +1,41 @@
-// cppdom was branched from the original LGPL'd xmlpp version 0.6
-// this new branched xmlpp is under the same LGPL (of course) and
-// is being maintained by:
-//    kevin meinert <subatomic@users.sf.net>
-//    allen bierbaum <allenb@users.sf.net>
-//    ben scott <nonchocoboy@users.sf.net>
-/////////////////////////////////////////////////////////////////////
-/*
-   xmlpp - an xml parser and validator written in C++
-   copyright (c) 2000-2001 Michael Fink
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public
-   License along with this library; if not, write to the
-   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA  02111-1307  USA.
-
-*/
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+/*************************************************************** cppdom-cpr beg
+ * 
+ * cppdom was forked from the original xmlpp version 0.6 under the LGPL. This
+ * new, branched xmlpp is under the same LGPL (of course) and is being
+ * maintained by:
+ *      Kevin Meinert   <subatomic@users.sourceforge.net>
+ *      Allen Bierbaum  <allenb@users.sourceforge.net>
+ *      Ben Scott       <nonchocoboy@users.sourceforge.net>
+ *
+ * -----------------------------------------------------------------
+ *
+ * xmlpp - an xml parser and validator written in C++
+ * copyright (c) 2000-2001 Michael Fink
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ ************************************************************ cppdom-cpr-end */
 #include "version.h"
 
 // These helper macros are used to stringify a given macro
