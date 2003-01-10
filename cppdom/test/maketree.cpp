c23eaacc3f682971e2842cdf3eede4cd407da37e
allenb <allenb@bdc45a6f-1a19-0410-8385-c3db3a2b84f1>
2003-01-10 21:48:18 +0000

    Fix bugs in test application.
    git-svn-id: https://xml-cppdom.svn.sourceforge.net/svnroot/xml-cppdom/trunk@227 bdc45a6f-1a19-0410-8385-c3db3a2b84f1

--- cppdom/test/maketree.cpp
+++ cppdom/test/maketree.cpp
@@ -13,7 +13,7 @@ int main()
    //   we will just define one here at the beginning and use it
    //   the entire way through
    cppdom::ContextPtr ctx( new cppdom::Context );
-   cppdom::Document doc("Document Root", ctx );   
+   cppdom::Document doc("Document", ctx );   
    
    // What it should look like
    // - Root
@@ -21,17 +21,22 @@ int main()
    //       cdata: This is element1
    //   - Element2:
    //      - Element3: attrib1:attrib1
+   cppdom::NodePtr root(new cppdom::Node("root", ctx));
    cppdom::NodePtr element1(new cppdom::Node("Element1", ctx));
    cppdom::NodePtr element2(new cppdom::Node("Element2", ctx));
    cppdom::NodePtr element3(new cppdom::Node("Element3", ctx));
    cppdom::NodePtr element1_cdata(new cppdom::Node("Element1-cdata", ctx));
 
+   // Document can only have one element as child (to be valid xml)
+   // Set this to the root element
+   doc.addChild(root);
+      
    // Now add element 1
    // - Also set it's attributes
-   doc.addChild(element1);
+   root->addChild(element1);
    element1->setAttribute("attrib1", 1);
    element1->setAttribute("attrib2", "two");
-   //element1->addChild(element1_cdata);
+   element1->addChild(element1_cdata);
 
    // Cdata must have it's type set
    // then set the actual contents of the cdata
@@ -39,7 +44,7 @@ int main()
    element1_cdata->setCdata("This is element1");
 
    // Add a couple of nested nodes and set the attributes
-   doc.addChild(element2);
+   root->addChild(element2);
    element2->addChild(element3);
    element3->setAttribute("attrib1", "attrib1");
 
@@ -48,6 +53,6 @@ int main()
 
    // Write the document out to a file
    doc.save(std::cout);
-   std::string filename = "maketree.xml";
+   std::string filename("maketree.xml");
    doc.saveFile(filename);   
 }
