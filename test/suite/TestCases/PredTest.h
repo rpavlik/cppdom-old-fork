b1fa8389ec5912f469f5e402b4a4a172d08c4444
allenb <allenb@bdc45a6f-1a19-0410-8385-c3db3a2b84f1>
2004-02-20 22:35:18 +0000

    Added predicate tests
    git-svn-id: https://xml-cppdom.svn.sourceforge.net/svnroot/xml-cppdom/trunk/cppdom@305 bdc45a6f-1a19-0410-8385-c3db3a2b84f1

---
+++ test/suite/TestCases/PredTest.h
@@ -0,0 +1,69 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+/*************************************************************** cppdom-cpr beg
+ *
+ * cppdom was forked from the original xmlpp version 0.6 under the LGPL. This
+ * new, branched xmlpp is under the same LGPL (of course) and is being
+ * maintained by:
+ *      Kevin Meinert   <subatomic@users.sourceforge.net>
+ *      Allen Bierbaum  <allenb@users.sourceforge.net>
+ *      Ben Scott       <nonchocoboy@users.sourceforge.net>
+ *
+ * -----------------------------------------------------------------
+ *
+ * xmlpp - an xml parser and validator written in C++
+ * copyright (c) 2000-2001 Michael Fink
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ ************************************************************ cppdom-cpr-end */
+#ifndef CPPDOM_TEST_PRED_TEST_H
+#define CPPDOM_TEST_PRED_TEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppdom/cppdom.h>
+
+namespace cppdomtest
+{
+
+class PredTest : public CppUnit::TestFixture
+{
+
+CPPUNIT_TEST_SUITE(PredTest);
+CPPUNIT_TEST(testHasAttributeNamePredicate);
+CPPUNIT_TEST(testHasAttributeValuePredicate);
+CPPUNIT_TEST_SUITE_END();
+
+public:
+
+   void testHasAttributeNamePredicate();
+
+   void testHasAttributeValuePredicate();
+
+public:
+   // Load the game.xml data file and return base document node
+   cppdom::Document loadGameDoc();
+};
+
+}
+
+#endif
---
+++ test/suite/TestCases/TestData.h
@@ -0,0 +1,10 @@
+#ifndef _TEST_DATA_H_
+#define _TEST_DATA_H_
+
+namespace cppdomtest
+{
+
+   const std::string game_xml_filename("data/game.xml");
+}
+      
+#endif
