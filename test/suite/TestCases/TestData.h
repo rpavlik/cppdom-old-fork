1d2a53c4bca726634d3620683cfa0991126a0c8d
allenb <allenb@bdc45a6f-1a19-0410-8385-c3db3a2b84f1>
2005-02-03 23:56:29 +0000

    - Added Node::hasChild method. - Rearranged ordering of Node interface in header - Added tests for node child methods - Formatting in test files.
    git-svn-id: https://xml-cppdom.svn.sourceforge.net/svnroot/xml-cppdom/trunk/cppdom@406 bdc45a6f-1a19-0410-8385-c3db3a2b84f1

--- test/suite/TestCases/TestData.h
+++ test/suite/TestCases/TestData.h
@@ -7,11 +7,12 @@ namespace cppdomtest
    const std::string game_xml_filename("data/game.xml");
    const std::string nodetest_xml_filename("data/nodetest.xml");
    const std::string simple_nodes_xml_filename("data/simple_nodes.xml");
-       
+   const std::string equal_test_xml_filename("data/equal_test.xml");
+
    const std::string rime_xml_filename("data/rime10.xml");
    const std::string hamlet_xml_filename("data/hamlet.xml");
-   
+
    const std::string xml_spec_filename("data//REC-xml-20040204.xml");
 }
-      
+
 #endif
