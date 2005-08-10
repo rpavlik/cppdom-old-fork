9c858552736228eeed3cf4beb4f8b5a59b96776e
allenb <allenb@bdc45a6f-1a19-0410-8385-c3db3a2b84f1>
2005-08-10 16:21:35 +0000

    After growing way to tired of waiting for sf.net to add svn support, I have just imported a copy of SConsAddons.  I am not using a vendor branch because I don't intend/want to make local modifications to the library.  Instead I plan on just having this track the current "release" of SConsAddons.  Once we have svn support at sf.net we could do this same thing using the svn:external property.
    git-svn-id: https://xml-cppdom.svn.sourceforge.net/svnroot/xml-cppdom/trunk/cppdom@430 bdc45a6f-1a19-0410-8385-c3db3a2b84f1

---
+++ tools/scons-addons/src/SConsAddons/Options/__init__.py
@@ -0,0 +1,41 @@
+"""SConsAddons.Options
+
+Main package for SConsAddons.Options extensions.
+
+Automatically imports the classes from Options.py.
+
+"""
+
+#
+# __COPYRIGHT__
+#
+# This file is part of scons-addons.
+#
+# Scons-addons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Scons-addons is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with scons-addons; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
+
+__version__ = "__VERSION__"
+
+__build__ = "__BUILD__"
+
+__buildsys__ = "__BUILDSYS__"
+
+__date__ = "__DATE__"
+
+__developer__ = "__DEVELOPER__"
+
+import SConsAddons.Options.Options
+from SConsAddons.Options.Options import Option, LocalUpdateOption, PackageOption, SimpleOption, Options
---
+++ tools/scons-addons/templates/code.py
@@ -0,0 +1,29 @@
+""" <filename>
+
+Docs
+
+"""
+
+#
+# __COPYRIGHT__
+#
+# This file is part of scons-addons.
+#
+# Scons-addons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Scons-addons is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with scons-addons; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
+
+
+import package
